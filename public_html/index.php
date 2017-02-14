cce9bd4083e2b88ff60dfc41d99b1efe856ccf44
Gauke Pieter Sietzema <gp@sterc.nl>
2017-02-14 11:36:27 +0100

    Changed head + logo + title

--- public_html/index.php
+++ public_html/index.php
@@ -1,38 +1,27 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="description" content="The MODX Bug Hunt is an international event where we squat as many MODX bugs as possible." />
+    <title>MODX Bug Hunt - March 3rd, 2017</title>
+    <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+    <link href="images/apple-icon.png" rel="apple-touch-icon">
+    <link href="images/apple-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="images/apple-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="images/apple-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
 
-<!-- Meta Tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-<meta name="description" content="NextEvent - Conference & Event Responsive HTML5 Template" />
-<meta name="keywords" content="building,business,construction,cleaning,transport,workshop" />
-<meta name="author" content="ThemeMascot" />
-
-<!-- Page Title -->
-<title>MODX Bug hunt</title>
-
-<!-- Favicon and Touch Icons -->
-<link href="images/favicon.png" rel="shortcut icon" type="image/png">
-<link href="images/apple-touch-icon.png" rel="apple-touch-icon">
-<link href="images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
-<link href="images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
-<link href="images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
-
-<!-- Stylesheet -->
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
-<link href="css/animate.css" rel="stylesheet" type="text/css">
-<link href="css/css-plugin-collections.css" rel="stylesheet"/>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/css-plugin-collections.css" rel="stylesheet"/>
 <!-- CSS | menuzord megamenu skins -->
 <link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-boxed.css" rel="stylesheet"/>
 <!-- CSS | Main style file -->
 <link href="css/style-main.css" rel="stylesheet" type="text/css">
 <!-- CSS | Theme Color -->
 <link href="css/colors/theme-skin-green.css" rel="stylesheet" type="text/css">
-<!-- CSS | Preloader Styles -->
-<link href="css/preloader.css" rel="stylesheet" type="text/css">
 <!-- CSS | Custom Margin Padding Collection -->
 <link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
 <!-- CSS | Responsive media queries -->
@@ -55,34 +44,16 @@
 <!-- Revolution Slider 5.x SCRIPTS -->
 <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
 <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
-
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 </head>
 <body class="">
 <div id="wrapper">
-  <!-- preloader -->
-  <div id="preloader">
-    <div id="spinner">
-      <div class="preloader-dot-loading">
-        <div class="cssload-loading"><i></i><i></i><i></i><i></i></div>
-      </div>
-    </div>
-    <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
-  </div>
-
   <!-- Header -->
   <header class="header">
     <div class="header-nav navbar-fixed-top navbar-dark navbar-transparent navbar-sticky-animated animated-active">
       <div class="header-nav-wrapper">
         <div class="container">
           <nav id="menuzord-right" class="menuzord green">
-            <a class="menuzord-brand pull-left flip" href="javascript:void(0)"><img src="images/logo-wide.png" alt=""></a>
+            <a class="menuzord-brand pull-left flip" href="javascript:void(0)"><img src="images/logo-modxbughunt.jpg" alt=""></a>
             <ul class="menuzord-menu">
               <li class="active"><a href="#home">Home</a></li>
               <li><a href="#about">about</a></li>
@@ -137,7 +108,7 @@
                   data-splitin="none"
                   data-splitout="none"
                   data-responsive_offset="on"
-                  style="z-index: 7; white-space: nowrap; font-weight:600;">Help making MODX bug free
+                  style="z-index: 7; white-space: nowrap; font-weight:600;">Help make MODX bug free
                 </div>
 
                 <!-- LAYER NR. 2 -->
@@ -163,7 +134,7 @@
                   data-splitin="none"
                   data-splitout="none"
                   data-responsive_offset="on"
-                  style="z-index: 5; white-space: nowrap; font-weight:600;">During MODX Bug hunt day
+                  style="z-index: 5; white-space: nowrap; font-weight:600;">MODX Bug Hunt
                 </div>
 
 
@@ -272,11 +243,11 @@
           <div class="col-md-12">
             <div class="divider layer-overlay overlay-deep" data-margin-top="-165px" data-bg-img="http://placehold.it/1920x1280">
               <div class="col-md-12 col-lg-8 p-50 pl-md-0 pr-0 md-text-center">
-                <h2 class="text-black-444">Meet Up For Free Registretion</h2>
+                <h2 class="text-black-444">Let's cleanse our issue tracker and join our MODX Bug Hunt on March 3rd, 2017</h2>
                 <ul class="list-inline xs-list-inline-none mt-50">
                   <li>
                     <h4 class="text-gray-light"><i class="fa fa-map-marker text-theme-colored"></i> Location</h4>
-                    <h6 class="text-gray">Khulna, Bangladesh</h6>
+                    <h6 class="text-gray">Sterc HQ or your own office</h6>
                   </li>
                   <li class="ml-30">
                     <h4 class="text-gray-light"><i class="fa fa-ticket text-theme-colored"></i> Tickets</h4>
