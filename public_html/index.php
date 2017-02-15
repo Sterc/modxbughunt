c36132345084b348b17716cc4edfe687d0a02bfc
Pien van Dalen <pien@sterc.nl>
2017-02-15 21:14:10 +0100

    Added padding around join us button

--- public_html/index.php
+++ public_html/index.php
@@ -39,7 +39,7 @@
 <script src="js/jquery-ui.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <!-- JS | jquery plugin collection for this theme -->
-<!--<script src="js/jquery-plugin-collection.js"></script>-->
+<<script src="js/jquery-plugin-collection.js"></script>
 
 <!-- Revolution Slider 5.x SCRIPTS -->
 <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
