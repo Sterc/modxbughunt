8dd46bb55e9332e8ba39200b8a344cf5890f9b34
Johan van der Molen <johan@pixelive.nl>
2017-03-03 07:51:27 +0000

    leaderboard.php created online with Bitbucket

---
+++ public_html/leaderboard.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Leaderboard - MODX Bug Hunt</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style>
+        header {
+            background-color: #323136;
+            padding: 10px 0;
+            text-align: center;
+        }
+
+        header img {
+            max-height: 100px;
+        }
+
+        footer {
+            background-color: #323136;
+            height: 100px;
+            margin-top: 100px;
+        }
+    </style>
+  </head>
+  <body>
+    <header>
+        <img src="http://www.modxbughunt.com/images/logo-modxbughunt-white-text.png" alt="Logo">
+    </header>
+    <div class="container">
+        <div class="pull-left">
+            <h1>Leaderboard</h1>
+            <p>The MODX bug hunt leaderboard. <br /><br /><a href="http://www.modxbughunt.com/" target="blank">Visit the website.</a></p>
+        </div>
+
+        <div class="pull-right">
+            <h3>Total issues fixed: 0</h3>
+        </div>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>
+                        Position
+                    </th>
+                    <th>
+                        Username
+                    </th>
+                    <th>
+                        Details
+                    </th>
+                    <th>
+                        Points
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        1
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        2
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        3
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        4
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        5
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        6
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        7
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        8
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        9
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        10
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        11
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        12
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        13
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        14
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        15
+                    </td>
+                    <td>
+                        John Doe
+                    </td>
+                    <td>
+                        Developing a fix: 0 times<br />
+                        Testing a fix: 0 times<br />
+                        Testing if an issue is correct: 0 times
+                    </td>
+                    <td>
+                        0
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <h1 class="text-center">Premium Sponsors</h1>
+        <div class="row">
+            <div class="col-xs-12 col-md-6 text-center">
+                <a href="https://www.sterc.nl/en/" target="_blank">
+                  <img src="http://www.modxbughunt.com/images/sponsor-sterc.png" alt="Sterc Internet &amp; Marketing" class="img-responsive">
+                </a>
+              </div>
+
+                <div class="col-xs-12 col-md-6 text-center">
+                    <a href="https://www.linqhost.nl" target="_blank">
+                        <img src="http://www.modxbughunt.com/images/sponsor-linqhost.png" alt="Linqhost" class="img-responsive">
+                    </a>
+                </div>
+
+              <div class="col-xs-12 col-md-6 text-center">
+                  <a href="https://seda.digital/" target="_blank">
+                      <img src="http://www.modxbughunt.com/images/sponsor-seda-digital2.png" alt="SEDA.digital" class="img-responsive"></a>
+              </div>
+
+                <div class="col-xs-12 col-md-6 text-center">
+                    <a href="https://www.digitalocean.com/" target="_blank">
+                        <img src="http://www.modxbughunt.com/images/sponsor-digitalocean.png" alt="DigitalOcean" class="img-responsive">
+                    </a>
+                </div>
+
+              <div class="col-xs-12 col-md-6 text-center">
+                  <a href="https://modmore.com" target="_blank"><img src="http://www.modxbughunt.com/images/sponsor-modmore2.png" alt="modmore" class="img-responsive"></a>
+              </div>
+          </div>
+          <h2 class="text-center">Sponsors</h2>
+          <div class="row">
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://greyskymedia.com/" target="_blank"><img src="http://www.modxbughunt.com/images/sponsor-greysky.png" alt="Grey Sky Media" class="img-responsive"></a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://www.heibelschoppen.nl" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-heibel.png" alt="Heibel" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://www.modx.com" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-modx.png" alt="MODX" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://www.gelstudios.co.uk/" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-gel-studios.jpg" alt="GEL Studios" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://modstore.pro" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-modstore.png" alt="MODSTORE.pro" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://modhost.pro" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-modhost.png" alt="MODHOST.pro" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://www.stickermule.com/" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-sticker-mule.png" alt="Sticker Mule" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="col-xs-6 col-md-3 text-center">
+                <a href="https://www.jetbrains.com" target="_blank">
+                    <img src="http://www.modxbughunt.com/images/sponsor-jetbrains.png" alt="Jetbrains" class="img-responsive">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <footer></footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
