c4bf85f3a488b9d2c9176eec0ec0ecbe76c81638
Uko van der Meulen <uko@sterc.nl>
2017-03-03 12:18:38 +0000

    leaderboard.php updated + time added

--- public_html/leaderboard.php
+++ public_html/leaderboard.php
@@ -44,7 +44,7 @@
         </div>
 
         <div class="pull-right">
-            <h3>Total issues fixed/closed: :) </h3>
+            <h3>Total issues fixed/closed: 37 [Local time: 13:17 CET] </h3>
         </div>
 
         <table class="table table-striped">
